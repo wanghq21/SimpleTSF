75c85394fb6b31f8ca50064c34649ea813fa1101
wanghq21 <32741441+wanghq21@users.noreply.github.com>
2025-02-09 13:32:43 +0800

    Update SimpleTSF.py

--- models/SimpleTSF.py
+++ models/SimpleTSF.py
@@ -2,13 +2,40 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-# from layers.Embed import DataEmbedding
-from layers.Autoformer_EncDec import series_decomp, series_decomp_multi
-
-from layers.SelfAttention_Family import AttentionLayer, ProbAttention, FullAttention
 import torch.nn.init as init
 
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
 
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
 
 class RMSNorm(nn.Module):
     def __init__(self, normalized_shape, eps=1e-8):
@@ -211,29 +238,10 @@ class Model(nn.Module):
 
         return enc_out 
 
-    # def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
-    #     # Normalization from Non-stationary Transformer
-    #     means = x_enc.mean(1, keepdim=True).detach()
-    #     x_enc = x_enc - means
-    #     stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-    #     x_enc /= stdev
-
-    #     _, L, N = x_enc.shape
-
-    #     for i in range(self.layer):
-    #         x_enc = self.model[i](x_enc)
-    #     enc_out = self.projection((x_enc ).transpose(1, 2)).transpose(1, 2)
-
-    #     enc_out = enc_out *stdev + means
-
-    #     return enc_out
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             dec_out  = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
             return dec_out[:, -self.pred_len:, :]   # [B, L, D]
-        if self.task_name == 'imputation':
-            dec_out = self.imputation(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
-            return dec_out  # [B, L, D]
         else:
             raise ValueError('Only forecast tasks implemented yet')
